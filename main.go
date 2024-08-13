503fdfce9b52f7f0f9473b4862cf4c6af1c901cf
Steve Dias <stevediaspro@gmail.com>
2024-08-13 18:32:24 +0200

    Adds info com

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	})
 
 	//Exemple of use of the couchdbUtils.ExpressRouterController
+
 	GrafanaCredentialRouterExemple(r)
 
 	handler := c.Handler(r)
@@ -39,30 +40,43 @@ func main() {
 // Exemple of use of the couchdbUtils.ExpressRouterController
 func GrafanaCredentialRouterExemple(router *mux.Router) {
 
-	var CoGrafanaCredentials *gocb.Collection
+	var CoGrafanaCredentials *gocb.Collection // is to set
 
-	var Cluster *gocb.Cluster
+	var Cluster *gocb.Cluster // is to set
 
 	// Exemple of use of the couchdbUtils.CreateCouchbaseContext
-	couchdbUtils.ExpressRouterController[entities.GrafanaCredentialsEntity, entities.GrafanaCredentialsEntity](Cluster, router, "/credentials", "credential_id", CoGrafanaCredentials, nil, false, func(r *http.Request, id string, newCredential *entities.GrafanaCredentialsEntity, isUpdate bool) (err error) {
-
-		if newCredential == nil {
-			return couchdbUtils.NewError(400, "Body is required")
-		}
+	//
+	// The function will create the GET /credentials, POST /credentials, GET /credentials/search, GET /credentials/{id}, PUT /credentials/{id}, DELETE /credentials/{id}
+	//
+	//couchdbUtils.CreateCouchbaseContext[<entity in DB>, <entity shown out of the router>](<Cluster Object>, <RouterObject>, <AtttributeIDName>, <CollectionObject>, <MidlewareFunc> ,<isActivate Middleware> ,<Create or Update function>)
+	couchdbUtils.ExpressRouterController[entities.GrafanaCredentialsEntity, entities.GrafanaCredentialsDto](Cluster, router, "/credentials", "credential_id", CoGrafanaCredentials, nil, false, func(r *http.Request, id string, newCredential *entities.GrafanaCredentialsEntity, isUpdate bool) (err error) {
 
 		// Check if the writing is an update or a new document
 		if !isUpdate {
+			// if is not update > then is a creation, we set the new id generated to the entity
 			newCredential.CredentialId = id
 		}
 
-		//
+		// ====================== Check Validity of the entity - BEGIN ======================
+		// Check if the body is empty
+		if newCredential == nil {
+			// if the body is empty, we return an error > then the entity be not saved
+			return couchdbUtils.NewError(400, "Body is required")
+		}
+
+		// Check if the required fields are filled
 		if newCredential.PasswordGrafana == "" {
+			// if the password is empty, we return an error > then the entity be not saved
 			return couchdbUtils.NewError(400, "Password is required")
 		}
 		if newCredential.UsernameGrafana == "" {
+			// if the password is empty, we return an error > then the entity be not saved
 			return couchdbUtils.NewError(400, "Username is required")
 		}
 
+		// ====================== Check Validity of the entity - END  ======================
+
+		// If all is ok, we return nil > then the entity will be saved
 		return nil
 	})
 
